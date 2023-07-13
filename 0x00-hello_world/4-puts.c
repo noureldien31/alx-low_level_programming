0af49ffe01c0ae5d8ddaa91b8c4ceb23424ea079
noureldien <noureldien31@gmail.com>
2023-07-13 16:28:11 -0700

    this is C low programming language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-		puts("\"programming is really cool");
-		return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
