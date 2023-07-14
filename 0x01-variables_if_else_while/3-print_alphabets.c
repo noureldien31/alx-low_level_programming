5f6e44a641a8bf38daf58f6668a9778b43bf749b
noureldien <noureldien31@gmail.com>
2023-07-14 10:21:02 -0700

    this is C variables,if else and loops

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: Print lower and upper abc.
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+	for (i = 65; i < 91; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
