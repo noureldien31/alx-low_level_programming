be2263a7fdaef1bfecf9b26c1fd9227117e8eabe
noureldien <noureldien31@gmail.com>
2023-07-14 10:08:26 -0700

    this is C variables,if else and loops

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Validate if the last number is greater than 5
+ *		less than 6 and not 0 or is 0.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, last;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	last = n % 10; /* get last number of n */
+
+	if (last != 0)
+	{
+		if (last > 5)
+			printf("Last digit of %d is %d and is greater than 5\n", n, last);
+		else	
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	}
+	else
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	return (0);
+}
