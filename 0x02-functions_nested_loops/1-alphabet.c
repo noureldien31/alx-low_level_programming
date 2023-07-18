249c77e534378b16d87b355ef2c3c12f31183244
noureldien <noureldien31@gmail.com>
2023-07-18 13:49:54 -0700

    this is C functions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prinT alphabet.
+ *
+ * Return: nothing
+ */
+void print_alphabet(void)
+{
+		int i;
+
+		for (i = 97; i < 123; i++)
+			_putchar(i);
+		_putchar('\n');
+}
