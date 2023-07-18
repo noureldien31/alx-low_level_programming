a326bcfb75afd0700f7666aa3600714868333a0f
noureldien <noureldien31@gmail.com>
2023-07-18 14:23:03 -0700

    this is C functions and nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character.
+ * @c: interger parammeter.
+ *
+ * Return: 0 or 1.
+ */
+int _isalpha(int c)
+{
+		if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+				return (1);
+		return (0);
+}
