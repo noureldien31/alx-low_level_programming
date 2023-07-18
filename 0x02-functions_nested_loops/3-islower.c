eae5e2d5ed2741e85d443ce8b99861a92c5a6237
noureldien <noureldien31@gmail.com>
2023-07-18 14:20:13 -0700

    this is C functions and nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character.
+ * @c: interger parammeter.
+ *
+ * Return: 0 or 1.
+ */
+int _islower(int c)
+{
+		if (c >= 97)
+			return (1);
+		return (0);
+}
