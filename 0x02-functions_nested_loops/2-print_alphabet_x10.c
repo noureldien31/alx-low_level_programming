828bf732c8df74707692df0f6c2f23967a969820
noureldien <noureldien31@gmail.com>
2023-07-18 14:06:00 -0700

    this is C functions and nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,10 @@
  */
 void print_alphabet_x10(void)
 {
-		int i, j;
+	int i, j;
 
-		for (i = 0; i < 10; i++)
-			for (j = 97; j < 123; j++)
-				_putchar(j);
-		_putchar('\n');
+	for (i = 0; i < 10; i++)
+		for (j = 97; j < 123; j++)
+			_putchar(j);
+			_putchar('\n');
 }
