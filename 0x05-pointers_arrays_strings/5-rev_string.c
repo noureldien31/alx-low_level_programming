7ebfe017f7988f87c11e6d5b64cbb102e4e6ee88
noureldien <noureldien31@gmail.com>
2023-07-24 16:14:01 -0700

    this is C pointers arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,27 +1,31 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
 /**
- * rev_string - reverses a string..
- * @s: char pointer.
+ * rev_string - This fuction print a string reverser
  *
+ *@s: this is the pointer that point to a string
  */
+
 void rev_string(char *s)
 {
-	int len, index;
-	char *arr;
+	int a, len;
 
-	while (s[len] != 0)
+	char *begin, *end = s;
+
+	for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
 	{
-		len++;
+		end++;
 	}
-	arr = (char *) malloc(len * sizeof(char));
-	for (index = 0; index <= len; index++)
+	len = a + 1;
+	begin = s;
+	for (a = 0; a < len / 2; a++)
 	{
-		arr[index] = (char) *(s + index);
+		char x;
+		x = *end;
+		*end = *begin;
+		*begin = x;
+		begin++;
+		end--;
 	}
-	for (index = 0; index <= len; index++)
-	{
-		*(s + index) = arr[len - index - 1];
-	}
+	end[len + 1] = '\0';
 }
