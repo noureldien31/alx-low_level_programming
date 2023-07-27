3e06a89b03f516dc865bcd935c716196359ea164
noureldien <noureldien31@gmail.com>
2023-07-26 17:02:04 -0700

    this is C pointers arrays and strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encoding into 1337speak
+ * @n: inpo
+ * Return: n value
+ */
+char *leet(char *n)
+{
+	int i, j;
+	char str1[] = "aAeEoOtTlL";
+	char str2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == str1[j])
+			{
+				n[i] = str2[j];
+			}
+		}
+	}
+	return (n);
+}
